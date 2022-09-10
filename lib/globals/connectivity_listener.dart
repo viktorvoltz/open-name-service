eeca1ac44a6be0d102590d4d966251bbeed08ffc
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-10 17:58:25 +0100

    global key

---
+++ lib/globals/connectivity_listener.dart
@@ -0,0 +1,39 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
+import '../repo/conectivity_status.dart';
+import '../service_locator.dart';
+import 'global_key.dart';
+
+Map<ConnectivityResult, dynamic> _source = {ConnectivityResult.none: false};
+NetworkConnectivityImpl listener = locator.get<NetworkConnectivityImpl>();
+String string = '';
+
+void connectivityListener(){
+  late bool isOnline;
+  locator.get<NetworkConnectivityImpl>().initialise();
+  listener.myStream.listen((event) {
+    isOnline = false;
+    _source = event;
+    print('source $_source');
+    
+    switch (_source.keys.toList()[0]) {
+      case ConnectivityResult.mobile:
+        isOnline = _source.values.toList()[0] ? true : false;
+        string =
+            _source.values.toList()[0] ? 'Mobile: Online' : 'Mobile: Offline';
+        break;
+      case ConnectivityResult.wifi:
+        string =
+            _source.values.toList()[0] ? 'WiFi: Online' : 'WiFi: Offline';
+        break;
+      case ConnectivityResult.none:
+      default:
+        string = 'Offline';
+    }
+
+    final SnackBar snackBar = SnackBar(content: Text(string), backgroundColor: isOnline ? Colors.green : Colors.red);
+    snackbarKey.currentState?.showSnackBar(snackBar);
+  });
+
+}

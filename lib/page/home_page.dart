8a9602fd736f9ab3b59867f6a1c45ee721aaa19b
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-10 17:57:41 +0100

    connectivity snack

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:webthreeauth/globals/connectivity_listener.dart';
+import 'package:webthreeauth/service_locator.dart';
 
 import '../repo/conectivity_status.dart';
 
@@ -12,65 +14,20 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  Map<dynamic, dynamic> _source = {ConnectivityResult.none: false};
-  final NetworkConnectivityImpl _networkConnectivity =
-      NetworkConnectivityImpl.instance;
-  String string = '';
-
   @override
   void initState() {
-    _networkConnectivity.initialise();
-  _networkConnectivity.myStream.listen((source) {
-    _source = source;
-    print('source $_source');
-    // 1.
-    switch (_source.keys.toList()[0]) {
-      case ConnectivityResult.mobile:
-        string =
-            _source.values.toList()[0] ? 'Mobile: Online' : 'Mobile: Offline';
-        break;
-      case ConnectivityResult.wifi:
-        string =
-            _source.values.toList()[0] ? 'WiFi: Online' : 'WiFi: Offline';
-        break;
-      case ConnectivityResult.none:
-      default:
-        string = 'Offline';
-    }
-    // 2.
-    setState(() {});
-    // 3.
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          string,
-          style: TextStyle(fontSize: 30),
-        ),
-      ),
-    );
-  });
+  connectivityListener();
     super.initState();
   }
 
   @override
   void dispose() {
-    _networkConnectivity.disposeStream();
+    locator.get<NetworkConnectivityImpl>().disposeStream();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        
-        backgroundColor: const Color(0xff6ae792),
-      ),
-      body: Center(
-          child: Text(
-        string,
-        style: TextStyle(fontSize: 54),
-      )),
-    );
+    return Scaffold();
   }
 }

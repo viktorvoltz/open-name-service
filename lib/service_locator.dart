1c4c8863864f60dc81209053f02959b6b5c910da
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-12 23:35:08 +0100

    register wallet connect provider

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
 
 import 'repo/conectivity_status.dart';
+import 'repo/wallet_connect_service.dart';
 
 final locator = GetIt.instance;
 
 void setup(){
   locator.registerLazySingleton<NetworkConnectivityImpl>(() => NetworkConnectivityImpl.instance);
+  locator.registerLazySingleton<WalletConnectService>(() => WalletConnectService());
 }

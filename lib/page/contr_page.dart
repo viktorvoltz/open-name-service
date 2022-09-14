ce943c3e7a862b48dbd173894c11f9454f0659f5
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-14 20:55:53 +0100

    feat buttons

--- lib/page/contr_page.dart
+++ lib/page/contr_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:webthreeauth/globals/custom_button.dart';
 import 'package:webthreeauth/service_locator.dart';
 
 import '../repo/contract.dart';
-import '../repo/wallet_connect_service.dart';
 import '../repo/wallet_connect_service.dart';
 
 class ContractPage extends StatefulWidget {
@@ -14,17 +14,12 @@ class ContractPage extends StatefulWidget {
 
 class _ContractPageState extends State<ContractPage> {
 
+  late DomainContract domainContract;
 
   @override
   void initState() {
-    kickUp();
+    domainContract = DomainContract();
     super.initState();
-  }
-
-  void kickUp() async{
-    DomainContract domainContract = DomainContract();
-    await locator.get<WalletConnectService>().connect();
-    await domainContract.registerDomain("victor-XX345");
   }
 
   @override
@@ -37,7 +32,27 @@ class _ContractPageState extends State<ContractPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomButton(
+                color: Colors.green,
+                text: "Connect Wallet",
+                onPressed: () async {
+                  await locator.get<WalletConnectService>().connect();
+              }),
+              const SizedBox(height: 30,),
+              CustomButton(
+                color: Colors.green,
+                text: "register domain name",
+                onPressed: () async{
+                  await domainContract.registerDomain("victor-XX345");
+              })
+            ]
+          ),
+      ),
     );
   }
 }

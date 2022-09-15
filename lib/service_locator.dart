bd99eadb7b2b8ff514bc6540e8171ad26b364b27
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-15 07:32:58 +0100

    add: domain contract  setup

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 
 import 'repo/conectivity_status.dart';
+import 'repo/contract.dart';
 import 'repo/wallet_connect_service.dart';
 
 final locator = GetIt.instance;
@@ -8,4 +9,5 @@ final locator = GetIt.instance;
 void setup(){
   locator.registerLazySingleton<NetworkConnectivityImpl>(() => NetworkConnectivityImpl.instance);
   locator.registerLazySingleton<WalletConnectService>(() => WalletConnectService());
+  locator.registerLazySingleton<DomainContract>(() => DomainContract());
 }

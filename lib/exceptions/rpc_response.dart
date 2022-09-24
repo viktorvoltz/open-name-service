52bbf6dd6abaedc4154676c86aa1476b7632a2b5
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-24 14:53:23 +0100

    feat: dynamic response

--- lib/exceptions/rpc_response.dart
+++ lib/exceptions/rpc_response.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../globals/global_key.dart';
 
-
 class RPCResponse<T> {
   bool succeed;
   T? data;
   String? error;
 
-  RPCResponse(this.data, {this.error}) : succeed = error == null{
-    if (!succeed){
-    final SnackBar snackBar = SnackBar(content: Text(error!));
-    snackbarKey.currentState?.showSnackBar(snackBar);
+  RPCResponse(this.data, {this.error}) : succeed = error == null {
+    if (!succeed) {
+      final SnackBar snackBar = SnackBar(content: Text(error!));
+      snackbarKey.currentState?.showSnackBar(snackBar);
+    } else {
+      final SnackBar snackBar = SnackBar(content: Text(data.toString()));
+      snackbarKey.currentState?.showSnackBar(snackBar);
     }
   }
 }

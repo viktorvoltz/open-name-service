65ad2d84ea31f4b959cb36c91133e355952115a4
viktorvoltz <vctrchinyeaka@gmail.com>
2022-09-24 22:48:57 +0100

    debug: contr screen

--- lib/page/contr_page.dart
+++ lib/page/contr_page.dart
@@ -7,7 +7,7 @@ import '../repo/contract.dart';
 import '../repo/wallet_connect_service.dart';
 
 class ContractPage extends StatefulWidget {
-  const ContractPage({ Key? key }) : super(key: key);
+  const ContractPage({Key? key}) : super(key: key);
 
   @override
   State<ContractPage> createState() => _ContractPageState();
@@ -35,7 +35,6 @@ class _ContractPageState extends State<ContractPage> {
     return null;
   }
 
-
   @override
   Widget build(BuildContext context) {
     final wcs = Provider.of<WalletConnectService>(context);
@@ -48,16 +47,21 @@ class _ContractPageState extends State<ContractPage> {
               Text(wcs.account),
               const SizedBox(height: 20),
               CustomButton(
-                color: Colors.green,
-                text: wcs.isConnected ? "connected" : "Connect Wallet",
-                onPressed: wcs.isConnected ? null : () async {
-                  //await locator.get<WalletConnectService>().connect();
-                  await wcs.connect();
-                  //account = locator.get<WalletConnectService>().getAccount();
-                  wcs.getAccount();
-              }),
-              const SizedBox(height: 30,),
+                  color: Colors.green,
+                  text: wcs.isConnected ? "connected" : "Connect Wallet",
+                  onPressed: wcs.isConnected
+                      ? null
+                      : () async {
+                          //await locator.get<WalletConnectService>().connect();
+                          await wcs.connect();
+                          //account = locator.get<WalletConnectService>().getAccount();
+                          wcs.getAccount();
+                        }),
+              const SizedBox(
+                height: 30,
+              ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Padding(
@@ -65,27 +69,29 @@ class _ContractPageState extends State<ContractPage> {
                     child: SizedBox(
                       width: 200,
                       child: TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                      hintText: 'Register a domain 🚀',
-                      errorText: _errorText,
-                ),
-              ),
+                        maxLength: 14,
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          hintText: 'Register a domain 🚀',
+                          errorText: _errorText,
+                        ),
+                      ),
                     ),
                   ),
-                  Container(
-                    width: 100,
+                  SizedBox(
+                    //width: 100,
                     child: CustomButton(
-                      color: Colors.green,
-                      text: "register domain name",
-                      onPressed: () async{
-                        await locator.get<DomainContract>().registerDomain(_nameController.text);
-                    }),
+                        color: Colors.green,
+                        text: "register",
+                        onPressed: () async {
+                          await locator
+                              .get<DomainContract>()
+                              .registerDomain(_nameController.text);
+                        }),
                   ),
                 ],
               )
-            ]
-          ),
+            ]),
       ),
     );
   }

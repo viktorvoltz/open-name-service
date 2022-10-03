ce4cee96692ab530731b62db3bd5d2d4e8fab607
viktorvoltz <vctrchinyeaka@gmail.com>
2022-10-03 10:44:21 +0100

    fix: contr_page

--- lib/page/contr_page.dart
+++ lib/page/contr_page.dart
@@ -5,6 +5,7 @@ import 'package:webthreeauth/service_locator.dart';
 
 import '../repo/contract.dart';
 import '../repo/wallet_connect_service.dart';
+import 'collection_web_view.dart';
 
 class ContractPage extends StatefulWidget {
   const ContractPage({Key? key}) : super(key: key);
@@ -45,97 +46,116 @@ class _ContractPageState extends State<ContractPage> {
       body: SafeArea(
         minimum: const EdgeInsets.only(top: 40, left: 10, right: 10),
         child: Center(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            const Text("Register a .voltz domain for 0.1 MATIC"),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(wcs.account),
-            const SizedBox(height: 20),
-            CustomButton(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Register a .voltz domain for 0.1 MATIC"),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(wcs.account),
+              const SizedBox(height: 20),
+              CustomButton(
+                  color: Colors.black,
+                  text: wcs.isConnected ? "connected" : "Connect Wallet",
+                  onPressed: wcs.isConnected
+                      ? null
+                      : () async {
+                          await wcs.connect();
+                          wcs.getAccount();
+                        }),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(
+                        maxLength: 14,
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          hintText: 'Register a domain 🚀',
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: CustomButton(
+                        color: Colors.black,
+                        text: "register",
+                        onPressed: () async {
+                          await locator
+                              .get<DomainContract>()
+                              .registerDomain(_nameController.text);
+                        }),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(
+                        controller: _address,
+                        decoration: const InputDecoration(
+                          hintText: 'check domain, E.g `john`',
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    //width: 100,
+                    child: CustomButton(
+                        color: Colors.black,
+                        text: "get Address",
+                        onPressed: () async {
+                          domainAddress = await locator
+                              .get<DomainContract>()
+                              .getAddress(_address.text);
+                          setState(() {});
+                        }),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text('(.)voltz domain owner:'),
+              const SizedBox(
+                height: 10,
+              ),
+              domainAddress == null
+                  ? Container()
+                  : Text(domainAddress[0].toString().contains('0x0000000000')
+                      ? '${_address.text}.voltz is unregistered yet'
+                      : '${_address.text}.voltz is owned by\n${domainAddress[0]}'),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomButton(
                 color: Colors.black,
-                text: wcs.isConnected ? "connected" : "Connect Wallet",
-                onPressed: wcs.isConnected
-                    ? null
-                    : () async {
-                        await wcs.connect();
-                        wcs.getAccount();
-                      }),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 200,
-                    child: TextField(
-                      maxLength: 14,
-                      controller: _nameController,
-                      decoration: const InputDecoration(
-                        hintText: 'Register a domain 🚀',
-                      ),
+                text: "Collections",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (ctx) => const CollectionWebView(),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  child: CustomButton(
-                      color: Colors.black,
-                      text: "register",
-                      onPressed: () async {
-                        await locator
-                            .get<DomainContract>()
-                            .registerDomain(_nameController.text);
-                      }),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: _address,
-                      decoration: const InputDecoration(
-                        hintText: 'check domain, E.g `john`',
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  //width: 100,
-                  child: CustomButton(
-                      color: Colors.black,
-                      text: "get Address",
-                      onPressed: () async {
-                        domainAddress = await locator
-                            .get<DomainContract>()
-                            .getAddress(_address.text);
-                        setState(() {});
-                      }),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text('(.)voltz domain owner:'),
-            const SizedBox(height: 10,),
-            domainAddress == null
-                ? Container()
-                : Text(domainAddress[0].toString().contains('0x0000000000')
-                    ? '${_address.text} is unregistered yet'
-                    : '${_address.text}.voltz is owned by\n${domainAddress[0]}')
-          ]),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );

88a2285f8591d2840360913dfbf39137ab7a9fc9
viktorvoltz <vctrchinyeaka@gmail.com>
2022-10-24 11:08:40 +0100

    refac

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Open DNS dApp',
       scaffoldMessengerKey: snackbarKey,
       theme: ThemeData(
         primarySwatch: Colors.blue,
